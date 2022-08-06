861e76c3ab1f7550abf15f16eada1aac1175af81
ahmed hamada <ahmedhamada496@gmail.com>
2022-08-06 04:18:11 +0200

    Inital commit Base for design

---
+++ Living.java
@@ -0,0 +1,19 @@
+public interface Living{
+       public void setName(String newme);
+       public String getName();
+
+       public void setheight(double heatleight);
+       public double getheight();
+
+       public void setWeight(double newWeight);
+       public double getWeight();
+
+       public void setFavfood(String newfood);
+       public String getFavFood();
+
+       public void setSpeed(double newspeed);
+       public double getSpeed();
+       
+       public void setSound(String newSound);
+       public String getSound();
+}
