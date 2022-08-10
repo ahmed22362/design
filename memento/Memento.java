e9be7f864514c043105efc03dc5edcfa21ca6d39
ahmed hamada <ahmedhamada496@gmail.com>
2022-08-10 06:34:51 +0200

    Adding memento design pattern as an atrical history

---
+++ memento/Memento.java
@@ -0,0 +1,17 @@
+package memento;
+
+public class Memento {
+
+    // The article stored in memento Object
+	
+	private String article;
+
+	// Save a new article String to the memento Object
+	
+	public Memento(String articleSave) { article = articleSave; }
+	
+	// Return the value stored in article 
+	
+	public String getSavedArticle() { return article; }
+	
+}
