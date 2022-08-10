f3f30e3e3aeb3362de20f40a7b09a3c93304e34e
ahmed hamada <ahmedhamada496@gmail.com>
2022-08-10 06:46:55 +0200

    Adding visitor pattern

---
+++ visitor/Visitor.java
@@ -0,0 +1,15 @@
+package visitor;
+
+interface Visitor {
+	
+	// Created to automatically use the right 
+	// code based on the Object sent
+	// Method Overloading
+	
+	public double visit(Liquor liquorItem);
+	
+	public double visit(Tobacco tobaccoItem);
+	
+	public double visit(Necessity necessityItem);
+	
+}
